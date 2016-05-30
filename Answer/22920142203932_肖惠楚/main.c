372958de2e4d18ec2ca8fb752c0a1d79b2a38824
VanillaChelle <617536227@qq.com>
2016-05-30 18:39:09 +0800

    finish Q3

---
+++ Answer/22920142203932_肖惠楚/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#include "include/my_stack.h"
+
+int main(void) {
+	int x;	
+
+	S.top = 0;
+	S.stacksize = 7;
+
+        while ( ! stack_is_full() ) {
+		scanf("%d", &x);
+		stack_push(x);
+	}
+	
+
+	while ( !stack_is_empty() ) {
+		x = stack_pop();
+
+		printf("%d\t", x);
+	}
+	printf("\n");
+
+	return 0;
+}
